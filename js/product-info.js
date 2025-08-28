0c53b59e1ff5b72564303568189e71af8d448501
FacundoBentancur <fbentancurcuadro@gmail.com>
2025-08-28 19:40:34 -0300

    Se quitó el botón salir de la navbar para dar lugar al nombre de usuario

--- js/product-info.js
+++
@@ -1,9 +0,0 @@
-// Logout
-const logoutBtn = document.getElementById("logoutBtn");
-if (logoutBtn) {
-  logoutBtn.addEventListener("click", function() {
-    localStorage.removeItem("loggedIn");
-    localStorage.removeItem("usuario");
-    window.location.href = "login.html";
-  });
-}
